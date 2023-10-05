3218ef21c7ce90fcd7a621bea63ed28f71dfddde
Thang Tran <bondshino.238@gmail.com>
2023-10-05 17:56:49 +0700

    update: set up config

--- app/res/colors.tsx
+++ app/res/colors.tsx
@@ -1,3 +1,8 @@
 export const colors = {
   //define colors here
+  primary: "#fefeff",
+  secondary: "#6B50F6",
+  substance: "#ffffff",
+  fontColor: "#000",
+  subFontColor: "#ccc"
 };
--- app/res/fonts.tsx
+++ app/res/fonts.tsx
@@ -1,3 +1,8 @@
 export const fonts = {
   //define fonts here
+  bold: 700,
+  normal: 400,
+  thin: 300,
+  color: "#000",
+  "sub-color": "#ccc"
 };
