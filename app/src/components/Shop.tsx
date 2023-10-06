a26f084e7ca7e33554970e486dd8de4d1d19b3d3
Thai Hoang <thaihoang20112k3@gmail.com>
2023-10-06 10:56:53 +0700

    feature/shop

---
+++ app/src/components/Shop.tsx
@@ -0,0 +1,61 @@
+import { StyleSheet, Text, View, Image, ImageSourcePropType } from 'react-native'
+import React from 'react'
+
+type Props = {
+    children?: React.ReactNode;
+    source?: {
+        image_url?: string | ImageSourcePropType;
+    };
+    text?: {
+        title?: string;
+        time?: string;
+    };
+};
+
+const Shop = (props: Props) => {
+    let imageSource = props.source?.image_url || '';
+    if (typeof imageSource === 'string') {
+        imageSource = { uri: imageSource };
+    }
+
+    const text = {
+        title: props.text?.title || '',
+        time: props.text?.time || '',
+    };
+    return (
+        <View style={styles.container}>
+            <Image source={imageSource} style={styles.image} />
+            <Text style={styles.title}>{text.title}</Text>
+            <Text style={styles.time}>{text.time}</Text>
+        </View>
+    );
+};
+
+export default Shop;
+
+const styles = StyleSheet.create({
+    container: {
+        width: 147,
+        height: 184,
+        borderRadius: 22,
+        backgroundColor: 'white',
+        alignContent: 'center',
+        justifyContent: 'center',
+
+    },
+    image: {
+        width: 100,
+        height: 100,
+        alignSelf: 'center',
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+    },
+    time: {
+        fontSize: 13,
+        color: 'grey',
+        alignSelf: 'center',
+    },
+});
--- app/src/scenes/Home.tsx
+++ app/src/scenes/Home.tsx
@@ -1,14 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
-
+import Shop from '../components/Shop'
 type Props = {}
 
 const Home = (props: Props) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Shop
+        source={{
+          image_url: require('../../../assets/Restaurant1.png')
+        }}
+        text={{
+          title: 'Product Title',
+          time: '13 minutes',
+        }}>
+      </Shop>
+      <Shop
+        source={{
+          image_url: require('../../../assets/Restaurant2.png')
+        }}
+        text={{
+          title: 'Product Title',
+          time: '13 minutes',
+        }}>
+      </Shop>
     </View>
   )
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  }
+});
