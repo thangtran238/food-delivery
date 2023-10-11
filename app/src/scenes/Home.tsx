308bea5b8d6b395a51cb24184a4167afb41525bc
Thai Hoang <thaihoang20112k3@gmail.com>
2023-10-11 09:57:45 +0700

    divide to banner

--- app/src/scenes/Home.tsx
+++ app/src/scenes/Home.tsx
@@ -1,54 +1,47 @@
-import { View, Text, ImageBackground, StyleSheet, Image } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
 import React from 'react'
-
+import Heading from '../components/Heading'
+import { GET_NAV } from '../util/constant'
+import SearchBarWithR from '../components/SearchBarWithR'
+import Banner from '../components/Banner'
+import Shop from '../components/Shop'
 type Props = {}
 
 const Home = (props: Props) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.imageBackground}>
+      <Heading navigation={GET_NAV()} />
+      <SearchBarWithR />
+      <Banner />
+
+      <Shop
+        source={{ image_url: require('../../res/image/Restaurant2.png') }}
+        text={{
+          title: 'Product Title',
+          time: '13 minutes',
+        }}
+      />
+
+
     </View>
   )
 }
 const styles = StyleSheet.create({
   imageBackground: {},
   image: { flex: 1 },
-  box_container: {
-    backgroundColor: '#6B50F6',
-    alignSelf: 'center',
-    marginTop: 20,
-    width: 325,
-    height: 150,
-    borderRadius: 16,
-    display: 'flex',
+  tex_container: {
     flexDirection: 'row',
-    // justifyContent: 'space-between',
+    justifyContent: 'space-around',
   },
-  text: {
-    color: 'white',
-    width: '50%',
-    marginRight: 10,
-    fontWeight: '700',
-    fontSize: 17,
+  nearest_restaurant: {
+    fontSize: 15,
+    color: 'black',
+    fontWeight: 'bold',
   },
-  box: {
-    // alignSelf: 'baseline',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
-    alignItems: 'baseline',
+  view_more: {
+    fontSize: 12,
+    color: '#6B50F6',
   },
-  button: {
-    marginLeft: 100,
-  },
-
 });
 
 export default Home
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  }
-});
