3ea03d03d7c767151a7133a89d11b8e477ca6c54
Hi <quan.dang24@student.passerellesnumeriques.org>
2023-11-10 16:34:46 +0700

    Im tired

--- app.config.js
+++ app.config.js
@@ -1,4 +1,5 @@
-{
+import 'dotenv/config';
+export default {
   "expo": {
     "name": "food-delivery",
     "slug": "food-delivery",
@@ -25,6 +26,15 @@
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    "extra":{
+      "apiKey": process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID
     }
   }
 }
